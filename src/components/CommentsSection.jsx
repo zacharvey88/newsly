508dc6e9f9056203a4a56f7f3125d716be148688
Zac <87400227+zacharvey88@users.noreply.github.com>
2024-05-08 17:28:51 +0100

    Functionality: View comments on article

--- src/components/CommentsSection.jsx
+++ src/components/CommentsSection.jsx
@@ -1,19 +1,36 @@
-export default () => {
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default ({article_id}) => {
+
+  const [comments, setComments] = useState([])
+
+useEffect(()=>{
+  axios.get(`https://nc-news-ngma.onrender.com/api/articles/${article_id}/comments`)
+  .then((response)=>{
+    setComments(response.data.comments)
+  })
+  .catch((err)=>{
+    console.log(err);
+  })
+})
 
   return (
     <section className="mb-5">
       <div className="card bg-light">
           <div className="card-body">
-              {/* <!-- Comment form--> */}
               <form className="mb-4"><textarea className="form-control" rows="3" placeholder="Join the discussion and leave a comment!"></textarea></form>
-              {/* <!-- Single comment--> */}
-              <div className="d-flex">
-                  <div className="flex-shrink-0"><img className="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
-                  <div className="ms-3">
-                      <div className="fw-bold">Commenter Name</div>
-                      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo quia quidem, odit cumque quibusdam officia natus ad, reprehenderit explicabo at, et unde atque. Quam, tempore sed consequuntur temporibus explicabo rerum.
+              {comments.map((comment)=>{
+                return (
+                  <div key={comment.comment_id} className="d-flex comment">
+                    <div className="flex-shrink-0"><img className="rounded-circle" src={comment.avatar_url} /></div>
+                    <div className="ms-3">
+                        <div className="fw-bold">{comment.author}<span className="comment-date">{comment.created_at.slice(0,16)}</span></div>
+                        {comment.body}
+                    </div>
                   </div>
-              </div>
+                )
+              })}
           </div>
       </div>
     </section>
