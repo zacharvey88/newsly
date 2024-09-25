271a3d06a8cc6929b8c9576b5a3cf028607b8fe0
Zac <87400227+zacharvey88@users.noreply.github.com>
2024-09-25 22:38:23 +0100

    style changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import TopicsBar from "./header-components/TopicsBar";
 
 export default () => {
   return (
-    <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', zIndex: 1000, backgroundColor: '#fff', boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)' }}>
+    <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', zIndex: 1000, backgroundColor: '#fff'}>
       <div style={{ maxWidth: '1200px', margin: '0 auto', padding: '10px 20px' }}>
         <Navbar />
         <TopicsBar />
