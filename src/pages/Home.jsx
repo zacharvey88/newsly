94796e459bfa42f2ae4e3c45424f1d940559a66e
Zac <87400227+zacharvey88@users.noreply.github.com>
2024-10-04 22:17:46 +0100

    Pagination button functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,31 +9,48 @@ import LoadingScreen from "../utilities/LoadingScreen";
 import Pagination from "../utilities/Pagination";
 
 export default () => {
+  const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalArticles, setTotalArticles] = useState(0);
 
-  const [articles, setArticles] = useState([])
-  const [isLoading,setIsLoading] = useState(true)
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get(`https://nc-news-ngma.onrender.com/api/articles?sort_by=votes&limit=10&p=${page}`)
+      .then((response) => {
+        setArticles(response.data.articles);
+        setTotalArticles(response.data.total_count); 
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+      });
+  }, [page]);
 
-  useEffect(()=>{
-    axios.get("https://nc-news-ngma.onrender.com/api/articles?sort_by=votes&limit=10")
-    .then((response)=>{
-      setArticles(response.data.articles)
-      setIsLoading(false)
-    })
-    .catch((err)=>{
-      console.log(err);
-    })
-  },[])
+  const handlePageChange = (newPage) => {
+    if (newPage >= 1 && newPage <= Math.ceil(totalArticles / 10)) {
+      setPage(newPage);
+    }
+  };
 
   return (
     <main className="container">
-      {isLoading ? <LoadingScreen /> : (
+      {isLoading ? (
+        <LoadingScreen />
+      ) : (
         <>
           <FeaturedArticleLarge article={articles[0]} />
           <FeaturedArticleSmall articles={articles.slice(1, 3)} />
           <div className="row g-5">
             <div className="col-md-8">
               <ArticlesList articles={articles.slice(3)} />
-              <Pagination />
+              <Pagination
+                currentPage={page}
+                totalArticles={totalArticles}
+                onPageChange={handlePageChange}
+              />
             </div>
             <div className="col-md-4">
               <div className="position-sticky">
@@ -46,5 +63,4 @@ export default () => {
       )}
     </main>
   );
-}
-
+};
