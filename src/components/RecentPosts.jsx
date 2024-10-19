d3ead5d4be022f9f9151a5425a892aa0b4a1a3cc
Zac <87400227+zacharvey88@users.noreply.github.com>
2024-10-19 22:44:54 +0100

    Fixing pagination

--- src/components/RecentPosts.jsx
+++ src/components/RecentPosts.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 export default ({ articles }) => {
-  if (!articles) {
+  if (!articles || articles.length === 0) {
     return <h3>Loading...</h3>;
   }
 
@@ -9,28 +9,28 @@ export default ({ articles }) => {
     <section>
       <h4 className="fst-italic">Most Commented Posts</h4>
       <ul className="list-unstyled">
-          return (
-            <li key={article.article_id}>
-              <Link
-                to={`/articles/${article.article_id}`}
-                className="d-flex flex-column flex-lg-row gap-3 align-items-start align-items-lg-center py-3 link-body-emphasis text-decoration-none border-top"
-              >
-                <img
-                  className="recent-article-img"
-                  width="100%"
-                  height="96"
-                  src={article.article_img_url}
-                  alt="Article picture"
-                ></img>
-                <div className="col-lg-8">
-                  <h6 className="mb-0">{article.title}</h6>
-                  <small className="text-body-secondary">
-                    {article.created_at.slice(0, 10)}
-                  </small>
-                </div>
-              </Link>
-            </li>
-          );
+        {articles.map((article) => (
+          <li key={article.article_id}>
+            <Link
+              to={`/articles/${article.article_id}`}
+              className="d-flex flex-column flex-lg-row gap-3 align-items-start align-items-lg-center py-3 link-body-emphasis text-decoration-none border-top"
+            >
+              <img
+                className="recent-article-img"
+                width="100%"
+                height="96"
+                src={article.article_img_url}
+                alt="Article picture"
+              />
+              <div className="col-lg-8">
+                <h6 className="mb-0">{article.title}</h6>
+                <small className="text-body-secondary">
+                  {article.created_at.slice(0, 10)}
+                </small>
+              </div>
+            </Link>
+          </li>
+        ))}
       </ul>
     </section>
   );
